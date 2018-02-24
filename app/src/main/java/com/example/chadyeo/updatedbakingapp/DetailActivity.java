c6b7c62c4385752fb2c282d303801722a64cfb70
Chad Yeo <chad.yeo.cy@gmail.com>
2018-02-24 14:13:44 -0500

    Added StepsListFragment

--- app/src/main/java/com/example/chadyeo/updatedbakingapp/DetailActivity.java
+++ app/src/main/java/com/example/chadyeo/updatedbakingapp/DetailActivity.java
@@ -1,7 +1,11 @@
 package com.example.chadyeo.updatedbakingapp;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.chadyeo.updatedbakingapp.fragments.StepsListFragment;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        StepsListFragment stepsListFragment = new StepsListFragment();
+
+        Bundle bundle = new Bundle();
+
+        fragmentTransaction.add(R.id.detailActivity_container, stepsListFragment);
+        fragmentTransaction.commit();
+
+        stepsListFragment.setArguments(bundle);
     }
 }
