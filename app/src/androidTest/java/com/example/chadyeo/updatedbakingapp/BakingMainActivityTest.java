fae8a3ff76e253574690c34db72dec9b333b1003
Chad Yeo <chad.yeo@sisa.samsung.com>
2018-04-04 15:33:45 -0400

    Fixed Espresso Test

--- app/src/androidTest/java/com/example/chadyeo/updatedbakingapp/BakingMainActivityTest.java
+++ app/src/androidTest/java/com/example/chadyeo/updatedbakingapp/BakingMainActivityTest.java
@@ -3,6 +3,8 @@ package com.example.chadyeo.updatedbakingapp;
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -13,36 +15,53 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.anything;
 
 
 @RunWith(AndroidJUnit4.class)
 public class BakingMainActivityTest {
 
-    private static final String RECIPE_NAME = "Nutella Pie";
-
-    private IdlingResource mIdlingResource;
+    final String RECIPE_NAME_ONE = "Nutella Pie";
 
     @Rule public ActivityTestRule<MainActivity> mMainActivityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
-    @Before
-    public void registerIdlingResource() {
-        mIdlingResource = mMainActivityTestRule.getActivity().getIdlingResource();
+    @Test
+    public void clickItem_recipeRecyclerView() {
 
-        Espresso.registerIdlingResources(mIdlingResource);
+        onView(withId(R.id.recipes_recyclerView))
+                .perform(RecyclerViewActions.actionOnItem(
+                        hasDescendant(withText(RECIPE_NAME_ONE)), click()));
     }
+
     @Test
     public void clickRecipeCardView_OpensDetailActivity(){
-        onData(anything()).inAdapterView(withId(R.id.recipes_recyclerView)).atPosition(0).perform(click());
+
+        Intents.init();
+
+        try {
+            onView(withId(R.id.recipes_recyclerView))
+                    .perform(RecyclerViewActions.actionOnItem(
+                            hasDescendant(withText(RECIPE_NAME_ONE)), click()));
+
+            intended(hasComponent(DetailActivity.class.getName()));
+        } finally {
+            Intents.release();
+        }
+
+        onView(withId(R.id.detailView_steps_recyclerView)).check(matches(
+                hasDescendant(allOf(isDisplayed(), withText("Recipe Introduction")))));
+
     }
 
-    @After
-    public void unregisterIdlingResource() {
-        if (mIdlingResource != null) {
-            Espresso.unregisterIdlingResources(mIdlingResource);
-        }
-    }
 }
